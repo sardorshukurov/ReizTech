658ce6da58de43cbe6c6d5dcdc5fd1ded9260f55
Sardor Shukurov <sshukurov@students.wiut.uz>
2023-03-17 23:46:17 +0500

    Task1 - Subject to improvement

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿int hours, minutes;
+int result = 0;
+
+Console.Write("Input hours: ");
+hours = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input minutes: ");
+minutes = Convert.ToInt32(Console.ReadLine());
+
+
+hours *= 30;
+minutes *= 6;
+
+if (hours > minutes)
+{
+    result = hours - minutes;
+}
+else
+{
+    result = minutes - hours;
+}
+
+if (result > 180)
+{
+    Console.WriteLine(360 - result);
+    Console.ReadLine();
+}
+Console.WriteLine(result);
