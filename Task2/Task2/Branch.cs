9ec5298f0a7772ea31e9039408738c6b0d7faafd
Sardor Shukurov <sshukurov@students.wiut.uz>
2023-03-18 11:19:08 +0500

    Task2 - Complete. Subject to change

---
+++ Task2/Task2/Branch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Branch
+    {
+        List<Branch> _branches;
+
+        public Branch() 
+        {
+            _branches = new List<Branch>();
+        }
+
+        public void AddBranch(Branch branch)
+        {
+            _branches.Add(branch);
+        }
+
+        public List<Branch> GetBranches()
+        {
+            return _branches;
+        }
+    }
+}
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,86 @@
+﻿using Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GenerateBranches();
+    }
+
+    static void GenerateBranches()
+    {
+        Branch root = new Branch();
+
+        Branch branch1 = new Branch();
+        Branch branch2 = new Branch();
+
+        root.AddBranch(branch1);
+        root.AddBranch(branch2);
+
+        Branch branch3 = new Branch();
+        Branch branch4 = new Branch();
+        Branch branch5 = new Branch();
+        Branch branch6 = new Branch();
+
+        branch1.AddBranch(branch3);
+        branch2.AddBranch(branch4);
+        branch2.AddBranch(branch5);
+        branch2.AddBranch(branch6);
+
+        Branch branch7 = new Branch();
+        Branch branch8 = new Branch();
+        Branch branch9 = new Branch();
+
+        branch4.AddBranch(branch7);
+        branch5.AddBranch(branch8);
+        branch5.AddBranch(branch9);
+
+        Branch branch10 = new Branch();
+
+        branch8.AddBranch(branch10);
+
+        List<Branch> allBranches = GetAllBranches(root);
+
+        foreach (Branch branch in allBranches)
+        {
+            int depth = GetDepth(root, branch);
+            Console.WriteLine($"Branch at depth {depth}: {branch}");
+        }
+    }
+
+    static List<Branch> GetAllBranches(Branch branch)
+    {
+        List<Branch> allBranches = new List<Branch>();
+        allBranches.Add(branch);
+
+        foreach (Branch childBranch in branch.GetBranches())
+        {
+            List<Branch> childBranches = GetAllBranches(childBranch);
+            allBranches.AddRange(childBranches);
+        }
+
+        return allBranches;
+    }
+
+
+    static int GetDepth(Branch root, Branch branch)
+    {
+        if (root == branch)
+        {
+            return 1;
+        }
+
+        foreach (Branch childBranch in root.GetBranches())
+        {
+            int depth = GetDepth(childBranch, branch);
+
+            if (depth != -1)
+            {
+                return depth + 1;
+            }
+        }
+
+        return -1;
+    }
+}
+
